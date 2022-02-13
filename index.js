b7714d0c272b1aba68a3ff97df322280fa9791d7
Shubham Mehra <mehrashubham18@gmail.com>
2022-02-13 16:17:37 +0530

    Typography updates

--- index.js
+++ index.js
@@ -12,30 +12,33 @@ const addNav = () => {
     </div>
     <div class="component-links-container">
         <ul class="component-links-list">
-            <li class="${activeLinkCheck(
-              "alert"
-            )}"><a href="/components/alert/alert.html">Alert</a></li>
-            <li class="${activeLinkCheck(
-              "avatar"
-            )}"><a href="/components/avatar/avatar.html">Avatar</a></li>
-            <li class="${activeLinkCheck(
-              "badge"
-            )}"><a href="/components/badges/badges.html">Badges</a></li>
-            <li class="${activeLinkCheck(
-              "button"
-            )}"><a href="/components/button/button.html">Buttons</a></li>
-            <li class="${activeLinkCheck(
-              "card"
-            )}"><a href="/components/card/card.html">Card</a></li>
-            <li class="${activeLinkCheck(
-              "image"
-            )}"><a href="/components/images/images.html">Images</a></li>
-            <li class="${activeLinkCheck(
-              "input"
-            )}"><a href="/components/inputs/input.html">Input</a></li>
-            <li class="${activeLinkCheck(
-              "toast"
-            )}"><a href="/components/toast/toast.html">Toast</a></li>
+            <li class="${activeLinkCheck("alert")}">
+              <a href="/components/alert/alert.html">Alert</a>
+            </li>
+            <li class="${activeLinkCheck("avatar")}">
+              <a href="/components/avatar/avatar.html">Avatar</a>
+            </li>
+            <li class="${activeLinkCheck("badge")}">
+              <a href="/components/badges/badges.html">Badges</a>
+            </li>
+            <li class="${activeLinkCheck("button")}">
+              <a href="/components/button/button.html">Buttons</a>
+            </li>
+            <li class="${activeLinkCheck("card")}">
+              <a href="/components/card/card.html">Card</a>
+            </li>
+            <li class="${activeLinkCheck("image")}">
+              <a href="/components/images/images.html">Images</a>
+            </li>
+            <li class="${activeLinkCheck("input")}">
+              <a href="/components/inputs/input.html">Input</a>
+            </li>
+            <li class="${activeLinkCheck("toast")}">
+              <a href="/components/toast/toast.html">Toast</a>
+            </li>
+            <li class="${activeLinkCheck("typography")}">
+              <a href="/components/typography/typography.html">Typography</a>
+            </li>
         </ul>
     </div>
 `;
